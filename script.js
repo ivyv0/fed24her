9c5ab4b80eede48b02b09fa2885b1df1667de6ae
ivyv0 <145538685+ivyv0@users.noreply.github.com>
2025-01-21 19:40:08 +0100

    ddff

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
-const menuButton = document.querySelector ("nav button");
+const menuButton = document.querySelector (".menu-toggle");
 const deNav = document.querySelector ("nav");
 menuButton.onclick = toggleMenu; 
 
 function toggleMenu () {
   deNav.classList.toggle("toonMenu");
-  
 }
 
 
